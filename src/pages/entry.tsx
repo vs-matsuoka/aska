5e65cef4526ccc1d56a9a90274deda1ff85a006a
naari3 <naari.named@gmail.com>
2023-03-12 12:06:52 +0900

    feat: add empty entry page

---
+++ src/pages/entry.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Footer from 'components/Footer';
+import LoopAnimationExample from 'components/LoopAnimationExample';
+import ParallaxExample from 'components/ParallaxExample';
+
+export default function Entry() {
+  return (
+    <div className="relative flex w-full items-center justify-center overflow-hidden"></div>
+  );
+}
