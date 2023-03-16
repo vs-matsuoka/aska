5025403a0e39134821b6af9d0fbfc90b4a644e9f
naari3 <naari.named@gmail.com>
2023-03-16 18:24:06 +0900

    feat: 白のきらめきをドロップシャドウ抜きにする

--- src/pages/entry.tsx
+++ src/pages/entry.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/future/image';
+import { useCallback, useState } from 'react';
+import { useSpring, animated, config } from 'react-spring';
 import Layout from 'components/Layout';
 
 type ContestantRowProps = {
@@ -25,9 +27,6 @@ function ContestantRow({ contestantImages, offset }: ContestantRowProps) {
             style={{
               clipPath: 'polygon(34% 25%, 88% 15%, 66% 75%, 12% 85%)'
             }}
-            onMouseOver={(e) => {
-              console.log(image);
-            }}
           />
         </div>
       ))}
@@ -35,57 +34,128 @@ function ContestantRow({ contestantImages, offset }: ContestantRowProps) {
   );
 }
 
+function ToggleCombiButton() {
+  const [styles, api] = useSpring(() => ({
+    from: { clipPath: "polygon(125% 0%, 175% 0%, 150% 100%, 100% 100%)" },
+    config: {
+      duration: 500,
+    },
+  }))
+
+  const [shadowOn, setShadowOn] = useState(true);
+
+  const trigger = useCallback(() => {
+    const handler = setTimeout(() => {
+      api.start({
+        from: { clipPath: "polygon(125% 0%, 175% 0%, 150% 100%, 100% 100%)" },
+        to: { clipPath: "polygon(-25% 0%, -15% 0%, -40% 100%, -50% 100%)" },
+      });
+    }, 150);
+    setShadowOn(false);
+    return () => clearTimeout(handler);
+  }, [api]);
+
+  const reset = useCallback(() => {
+    // const handler = setTimeout(() => {
+    setShadowOn(true);
+    // }, 150);
+    // return () => clearTimeout(handler);
+  }, []);
+
+  return (
+    <div className="absolute right-[3rem] bottom-[-8rem]">
+      <Image
+        alt="conbi"
+        src="/Entry/21_Entry_text_02_base.png"
+        width={480}
+        height={105}
+      />
+      <Image
+        alt="conbi"
+        className={"absolute top-0 left-0 " + (shadowOn ? "" : "hidden")}
+        src="/Entry/21_Entry_text_02_shadow.png"
+        width={480}
+        height={105}
+      />
+      <div className="absolute top-0 left-0 transition hover:scale-125"
+        onMouseEnter={trigger} onMouseLeave={reset}>
+        <animated.div
+          className="absolute top-0 left-0"
+          style={{
+            filter: 'brightness(0) invert(1)',
+            ...styles,
+          }}
+        >
+          <Image
+            alt="conbi"
+            src="/Entry/21_Entry_text_02.png"
+            width={480}
+            height={105}
+          />
+        </animated.div>
+        <Image
+          alt="conbi"
+          src="/Entry/21_Entry_text_02.png"
+          width={480}
+          height={150}
+        />
+      </div>
+    </div>
+  );
+}
+
+const contestantImageRows = [
+  [
+    '/Entry/Frame/21_Entry_Frame_01.png',
+    '/Entry/Frame/21_Entry_Frame_02.png',
+    '/Entry/Frame/21_Entry_Frame_03.png',
+    '/Entry/Frame/21_Entry_Frame_04.png',
+    '/Entry/Frame/21_Entry_Frame_05.png',
+    '/Entry/Frame/21_Entry_Frame_06.png',
+    '/Entry/Frame/21_Entry_Frame_07.png',
+    '/Entry/Frame/21_Entry_Frame_08.png',
+    '/Entry/Frame/21_Entry_Frame_09.png',
+    '/Entry/Frame/21_Entry_Frame_10.png'
+  ],
+  [
+    '/Entry/Frame/21_Entry_Frame_11.png',
+    '/Entry/Frame/21_Entry_Frame_12.png',
+    '/Entry/Frame/21_Entry_Frame_13.png',
+    '/Entry/Frame/21_Entry_Frame_14.png',
+    '/Entry/Frame/21_Entry_Frame_15.png',
+    '/Entry/Frame/21_Entry_Frame_16.png',
+    '/Entry/Frame/21_Entry_Frame_17.png',
+    '/Entry/Frame/21_Entry_Frame_18.png',
+    '/Entry/Frame/21_Entry_Frame_19.png',
+    '/Entry/Frame/21_Entry_Frame_20.png'
+  ],
+  [
+    '/Entry/Frame/21_Entry_Frame_21.png',
+    '/Entry/Frame/21_Entry_Frame_22.png',
+    '/Entry/Frame/21_Entry_Frame_23.png',
+    '/Entry/Frame/21_Entry_Frame_24.png',
+    '/Entry/Frame/21_Entry_Frame_25.png',
+    '/Entry/Frame/21_Entry_Frame_26.png',
+    '/Entry/Frame/21_Entry_Frame_27.png',
+    '/Entry/Frame/21_Entry_Frame_28.png',
+    '/Entry/Frame/21_Entry_Frame_29.png',
+    '/Entry/Frame/21_Entry_Frame_30.png'
+  ],
+  [
+    '/Entry/Frame/21_Entry_Frame_31.png',
+    '/Entry/Frame/21_Entry_Frame_32.png',
+    '/Entry/Frame/21_Entry_Frame_33.png',
+    '/Entry/Frame/21_Entry_Frame_34.png',
+    '/Entry/Frame/21_Entry_Frame_35.png',
+    '/Entry/Frame/21_Entry_Frame_36.png',
+    '/Entry/Frame/21_Entry_Frame_37.png',
+    '/Entry/Frame/21_Entry_Frame_38.png',
+    '/Entry/Frame/21_Entry_Frame_39.png',
+    '/Entry/Frame/21_Entry_Frame_40.png'
+  ]
+];
+
 export default function Entry() {
-  const contestantImageRows = [
-    [
-      '/Entry/Frame/21_Entry_Frame_01.png',
-      '/Entry/Frame/21_Entry_Frame_02.png',
-      '/Entry/Frame/21_Entry_Frame_03.png',
-      '/Entry/Frame/21_Entry_Frame_04.png',
-      '/Entry/Frame/21_Entry_Frame_05.png',
-      '/Entry/Frame/21_Entry_Frame_06.png',
-      '/Entry/Frame/21_Entry_Frame_07.png',
-      '/Entry/Frame/21_Entry_Frame_08.png',
-      '/Entry/Frame/21_Entry_Frame_09.png',
-      '/Entry/Frame/21_Entry_Frame_10.png'
-    ],
-    [
-      '/Entry/Frame/21_Entry_Frame_11.png',
-      '/Entry/Frame/21_Entry_Frame_12.png',
-      '/Entry/Frame/21_Entry_Frame_13.png',
-      '/Entry/Frame/21_Entry_Frame_14.png',
-      '/Entry/Frame/21_Entry_Frame_15.png',
-      '/Entry/Frame/21_Entry_Frame_16.png',
-      '/Entry/Frame/21_Entry_Frame_17.png',
-      '/Entry/Frame/21_Entry_Frame_18.png',
-      '/Entry/Frame/21_Entry_Frame_19.png',
-      '/Entry/Frame/21_Entry_Frame_20.png'
-    ],
-    [
-      '/Entry/Frame/21_Entry_Frame_21.png',
-      '/Entry/Frame/21_Entry_Frame_22.png',
-      '/Entry/Frame/21_Entry_Frame_23.png',
-      '/Entry/Frame/21_Entry_Frame_24.png',
-      '/Entry/Frame/21_Entry_Frame_25.png',
-      '/Entry/Frame/21_Entry_Frame_26.png',
-      '/Entry/Frame/21_Entry_Frame_27.png',
-      '/Entry/Frame/21_Entry_Frame_28.png',
-      '/Entry/Frame/21_Entry_Frame_29.png',
-      '/Entry/Frame/21_Entry_Frame_30.png'
-    ],
-    [
-      '/Entry/Frame/21_Entry_Frame_31.png',
-      '/Entry/Frame/21_Entry_Frame_32.png',
-      '/Entry/Frame/21_Entry_Frame_33.png',
-      '/Entry/Frame/21_Entry_Frame_34.png',
-      '/Entry/Frame/21_Entry_Frame_35.png',
-      '/Entry/Frame/21_Entry_Frame_36.png',
-      '/Entry/Frame/21_Entry_Frame_37.png',
-      '/Entry/Frame/21_Entry_Frame_38.png',
-      '/Entry/Frame/21_Entry_Frame_39.png',
-      '/Entry/Frame/21_Entry_Frame_40.png'
-    ]
-  ];
   return (
     <Layout withVignette>
       <div className="relative">
@@ -104,21 +174,7 @@ export default function Entry() {
             offset={index * 46}
           />
         ))}
-        <div className="absolute right-[3rem] bottom-[-8rem]">
-          <Image
-            alt="conbi"
-            src="/Entry/21_Entry_text_02_base.png"
-            width={600}
-            height={200}
-          />
-          <Image
-            alt="conbi"
-            className="absolute top-0 left-0"
-            src="/Entry/21_Entry_text_02.png"
-            width={600}
-            height={200}
-          />
-        </div>
+        <ToggleCombiButton />
       </div>
     </Layout>
   );
