83fdda590f3d002a7cf3389e7a59887857266522
naari3 <naari.named@gmail.com>
2023-03-17 23:19:34 +0900

    fix(storybook): 必須の引数を指定する

--- src/components/Background/stories.tsx
+++ src/components/Background/stories.tsx
@@ -6,4 +6,4 @@ export default {
   component: Background
 } as Meta
 
-export const Default: Story = () => <Background />
+export const Default: Story = () => <Background src="/Entry/21_Entry_pic_BG.png" />
