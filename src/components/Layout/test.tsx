3ce666a0eced18dd3170296e761af97b5308bcc5
naari3 <naari.named@gmail.com>
2023-03-24 04:09:07 +0900

    chore: eslintの設定を戻す

--- src/components/Layout/test.tsx
+++ src/components/Layout/test.tsx
@@ -6,10 +6,14 @@ jest.mock('next/router', () => ({ useRouter: jest.fn() }));
 
 describe('<Layout />', () => {
   it('正しくレンダリングされる', async () => {
-    render(<Layout><h1>AAAAAAAAAAAAAA</h1></Layout>);
+    render(
+      <Layout>
+        <h1>AAAAAAAAAAAAAA</h1>
+      </Layout>
+    );
 
-    const { getByText } = within(screen.getByTestId("children"));
+    const { getByText } = within(screen.getByTestId('children'));
 
-    expect(getByText("AAAAAAAAAAAAAA")).toBeInTheDocument();
+    expect(getByText('AAAAAAAAAAAAAA')).toBeInTheDocument();
   });
 });
--- src/pages/contestants.tsx
+++ src/pages/contestants.tsx
@@ -15,7 +15,7 @@ function ContestantBox({ text, odd, shifting }: ContestantBoxProps) {
   const image = odd ? '/dog.png' : '/cat.png';
   return (
     <div
-      className={`h-24 w-24 -skew-x-6 border-4 border-mdmGold-100 ${bg} ${shift} shadow-xl`}
+      className={`border-mdmGold-100 h-24 w-24 -skew-x-6 border-4 ${bg} ${shift} shadow-xl`}
     >
       <div className="fixed top-[-5px] left-[-5px] h-24 w-24 skew-x-6 skew-y-6">
         <Image
@@ -41,7 +41,7 @@ function CombinationBox({ text1, text2, shifting }: CombinationBoxProps) {
   const shift = shifting ? '' : 'translate-x-[-25%]';
   return (
     <div className={`col-span-2 flex -skew-x-6 shadow-xl ${shift}`}>
-      <div className="h-24 w-28 border-y-4 border-l-4 border-mdmGold-100 bg-mdmRed">
+      <div className="border-mdmGold-100 h-24 w-28 border-y-4 border-l-4 bg-mdmRed">
         <div className="fixed top-[0] left-[0] h-24 w-24 skew-x-6 skew-y-6">
           <Image
             src={useAssetPath('/dog.png')}
@@ -52,7 +52,7 @@ function CombinationBox({ text1, text2, shifting }: CombinationBoxProps) {
         </div>
         {text1}
       </div>
-      <div className="h-24 w-28 border-y-4 border-r-4 border-mdmGold-100 bg-mdmBlue">
+      <div className="border-mdmGold-100 h-24 w-28 border-y-4 border-r-4 bg-mdmBlue">
         <div className="fixed top-[0] left-[107px] h-24 w-24 skew-x-6 skew-y-6">
           <Image
             src={useAssetPath('/cat.png')}
