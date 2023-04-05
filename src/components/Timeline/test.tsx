377006256d877742a97764f84ed2c1839cc2e503
gobou0516 <gobou.mp0516.vvv@ezweb.ne.jp>
2023-04-05 16:30:51 +0000

    test: change Timeline test

--- src/components/Timeline/test.tsx
+++ src/components/Timeline/test.tsx
@@ -3,12 +3,10 @@ import { render, screen } from '@testing-library/react';
 import Timeline from '.';
 
 describe('<Timeline />', () => {
-  it('should render the heading', () => {
+  it('should render the link', () => {
     const { container } = render(<Timeline />);
 
-    expect(
-      screen.getByRole('heading', { name: /Timeline/i })
-    ).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
   });
