c4c6f26a29c3971a3f3c50d5e6da3996861110f5
gobou0516 <gobou.mp0516.vvv@ezweb.ne.jp>
2023-05-02 11:01:48 +0000

    chore: change the layout of the menu bar

--- src/components/MenuBar/index.tsx
+++ src/components/MenuBar/index.tsx
@@ -15,24 +15,26 @@ const MenuBar = () => (
       />
     </div>
     <div className="absolute inset-1/2 top-[36px] flex flex-row items-center justify-center space-x-[37px]">
+      <Link href="/" passHref>
+        <a>
+          <Image src="/MenuBar/00_Menu_Text_01.png" alt="top" className="max-w-none" width={150} height={50} />
+        </a>
+      </Link>
       <Link href="/about" passHref>
-        <a href="#">
-          <Image src="/MenuBar/00_Menu_Text_01.png" alt="overview" className="max-w-none" width={150} height={50} />
+        <a>
+          <Image src="/MenuBar/00_Menu_Text_02.png" alt="overview" className="max-w-none" width={150} height={50} />
         </a>
       </Link>
       <Link href="/entries" passHref>
         <a>
-          <Image src="/MenuBar/00_Menu_Text_02.png" alt="entries" className="max-w-none" width={150} height={50} />
+          <Image src="/MenuBar/00_Menu_Text_03.png" alt="entries" className="max-w-none" width={150} height={50} />
         </a>
       </Link>
       <a href="#">
-        <Image src="/MenuBar/00_Menu_Text_03.png" alt="movie" className="max-w-none" width={150} height={50} />
+        <Image src="/MenuBar/00_Menu_Text_04.png" alt="movie" className="max-w-none" width={150} height={50} />
       </a>
       <a href="#">
-        <Image src="/MenuBar/00_Menu_Text_04.png" alt="operation" className="max-w-none" width={150} height={50} />
-      </a>
-      <a href="#">
-        <Image src="/MenuBar/00_Menu_Text_05.png" alt="unknown" className="max-w-none" width={150} height={50} />
+        <Image src="/MenuBar/00_Menu_Text_05.png" alt="operation" className="max-w-none" width={150} height={50} />
       </a>
     </div>
   </div>
