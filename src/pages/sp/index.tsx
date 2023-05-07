003e093d826ac2b408cacf33bf82001787b8f8c9
gobou0516 <gobou.mp0516.vvv@ezweb.ne.jp>
2023-05-07 11:06:31 +0000

    chore: adjust sp-top placement

--- src/pages/sp/index.tsx
+++ src/pages/sp/index.tsx
@@ -4,18 +4,16 @@ import SpResponsiveImage from 'components/SpResponsiveImage';
 
 export default function Top() {
   return (
-    <div className="">
+    <div className="relative w-screen overflow-hidden">
       <Background src="/SP/Top/s11_Top_BG.png" />
-      <div className="absolute">
-        <div className="relative w-screen overflow-hidden">
-          {/* sp:top-[40px] sp:left-[-388px] */}
-          <div className="absolute top-[5.3333333333vw] left-[-51.7333333333vw]">
-            <SpResponsiveImage src="/SP/Top/s11_Top_KeyVisual_02.png" alt="zeus" className="relative" width={1050} height={1050} />
-          </div>
-          {/* sp:top-[116px] sp:right-[470px] */}
-          <div className="absolute top-[15.4666666667vw] right-[62.6666666667vw]">
-            <SpResponsiveImage src="/SP/Top/s11_Top_KeyVisual_01.png" alt="amatelas" className="relative" width={1050} height={1050} />
-          </div>
+      <div className="">
+        {/* sp:top-[40px] sp:left-[-388px] */}
+        <div className="absolute top-[5.3333333333vw] left-[-51.7333333333vw]">
+          <SpResponsiveImage src="/SP/Top/s11_Top_KeyVisual_02.png" alt="zeus" className="relative" width={1050} height={1050} />
+        </div>
+        {/* sp:top-[116px] sp:right-[470px] */}
+        <div className="absolute top-[15.4666666667vw] right-[-62.6666666667vw]">
+          <SpResponsiveImage src="/SP/Top/s11_Top_KeyVisual_01.png" alt="amatelas" className="relative" width={1050} height={1050} />
         </div>
       </div>
       <Background src="/SP/Top/s11_Top_GradOverlay.png" />
@@ -36,9 +34,10 @@ export default function Top() {
           <SpResponsiveImage src="/SP/Top/s11_Top_Button_02_SNS_YouTube.png" alt="youtube" className="relative" width={380} height={48} />
           <SpResponsiveImage src="/SP/Top/s11_Top_Button_03_SNS_Twitter.png" alt="twitter" className="relative" width={380} height={48} />
         </div>
-        {/* sp:mt-[106px] sp:space-y-[16px] */}
-        <div className="mx-4 mt-[14.1333333333vw] grid justify-items-center space-y-[2.133333333333333px]">
-          <div className="flex gap-[16px]">
+        {/* sp:mx-[32px] sp:mt-[106px] */}
+        <div className="mx-[4.2666666666667vw] mt-[14.1333333333vw] grid justify-items-center">
+          {/* sp:gap-[16px] */}
+          <div className="flex gap-[2.133333333333333vw]">
             <Link href="/sp/entries" passHref>
               <a>
                 <SpResponsiveImage src="/SP/Top/s11_Top_Button_11_Concept.png" alt="entry" className="relative" width={335} height={192} />
@@ -50,8 +49,8 @@ export default function Top() {
               </a>
             </Link>
           </div>
-          {/* sp:top-[10px] sp:gap-[16px] */}
-          <div className="top-[1.333333333333333vw] flex gap-[2.133333333333333vw]">
+          {/* sp:mt-[16px] sp:gap-[16px] */}
+          <div className="mt-[2.133333333333333vw] flex gap-[2.133333333333333vw]">
             <SpResponsiveImage src="/SP/Top/s11_Top_Button_13_Unei.png" alt="overview" className="relative" width={218} height={128} />
             <SpResponsiveImage src="/SP/Top/s11_Top_Button_14_Movie.png" alt="unei" className="relative" width={218} height={128} />
             <SpResponsiveImage src="/SP/Top/s11_Top_Button_15_Secret.png" alt="movie" className="relative" width={218} height={128} />
