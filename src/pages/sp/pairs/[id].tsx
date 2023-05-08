cca7d56763636ba3dba3707ecda307f03c7ca355
gobou0516 <gobou.mp0516.vvv@ezweb.ne.jp>
2023-05-08 13:36:38 +0000

    fix: keshiwasureta

--- src/pages/sp/pairs/[id].tsx
+++ src/pages/sp/pairs/[id].tsx
@@ -19,8 +19,6 @@ function getPairIcon(pair: Pair, index: number) {
 
 type PairProps = InferGetStaticPropsType<typeof getStaticProps>;
 
-export type Param = {};
-
 export const getStaticPaths: GetStaticPaths = async () => {
   const noSecretPairs = pairs.filter((pair) => !pair.spIconSrc.includes('secret'));
 
