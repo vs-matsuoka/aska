6f55a4548ddc9bf213571eabc39148b6abb9be36
naari3 <naari.named@gmail.com>
2023-05-13 02:03:26 +0900

    fix: コンビページの各座標を出場者のものに合わせる

--- src/pages/pairs.tsx
+++ src/pages/pairs.tsx
@@ -67,7 +67,16 @@ function PairButton({ pair, onModalOpen }: { pair: Pair; onModalOpen: () => void
 }
 
 function PairRow({ pairs, offset, onModalOpen }: PairRowProps) {
-  const isNarrow = useMediaQuery({ query: '(min-width: 1920px)' });
+  const [isNarrow, setIsNarrow] = useState(false);
+  useEffect(() => {
+    setIsNarrow(window.innerWidth > 1920);
+    const updateWindowDimensions = () => {
+      setIsNarrow(window.innerWidth > 1920);
+    };
+
+    window.addEventListener('resize', updateWindowDimensions);
+    return () => window.removeEventListener('resize', updateWindowDimensions);
+  }, []);
 
   return (
     <div
@@ -112,12 +121,11 @@ function ToggleEntriesButton() {
   }, []);
 
   return (
-    <div className="absolute right-[2.5vw] bottom-[-6.5vw] 4xl:right-[3rem] 4xl:bottom-[-8rem]">
-      <ResponsiveImage alt="conbi" src="/Pair/31_Pair_text_02_base.png" width={480} height={105} />
-      <ResponsiveImage alt="conbi" className={'absolute top-0 left-0 ' + (shadowOn ? '' : 'hidden')} src="/Pair/31_Pair_text_02_shadow.png" width={480} height={105} />
+    <div className="absolute right-[4.1666666667vw] bottom-[-5.46875vw] 4xl:right-[80px] 4xl:bottom-[-105px]">
+      <ResponsiveImage alt="conbi" src="/Pair/31_Pair_text_02_base.png" width={500} height={105} />
       <Link href="/entries" passHref>
         <div className="absolute top-0 left-0 transition hover:scale-125" onMouseEnter={trigger} onMouseLeave={reset}>
-          <ResponsiveImage alt="conbi" src="/Pair/31_Pair_text_02.png" width={480} height={105} />
+          <ResponsiveImage alt="conbi" src="/Pair/31_Pair_text_02.png" width={500} height={105} />
           <animated.div
             className="absolute top-0 left-0"
             style={{
@@ -273,8 +281,8 @@ const Pairs: NextPageWithLayout = () => {
           {/* <Head></Head> */}
           <div className="relative my-auto h-[46.875vw] w-[82.8125vw] 4xl:h-[900px] 4xl:w-[1590px]">
             <div className="relative">
-              <div className="absolute top-[8.8541666667vw] 4xl:top-[170px]">
-                <div className="absolute left-[1.9vw] top-[-10vw] 4xl:left-[36.48px] 4xl:top-[-211.2px]">
+              <div className="absolute top-[10vw] left-[1.6666666667vw] 4xl:top-[192px] 4xl:left-[32px]">
+                <div className="absolute left-[0.2604166667vw] top-[-12.1354166667vw] 4xl:left-[5px] 4xl:top-[-233px]">
                   <ResponsiveImage alt="pair" src="/Pair/31_Pair_text_01.png" width={500} height={250} />
                 </div>
                 {pairRows.map((row, index) => (
