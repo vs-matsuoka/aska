8e27771f630304b71d46ce2641202c6bd0a50a3e
gobou0516 <gobou.mp0516.vvv@ezweb.ne.jp>
2023-05-12 14:38:26 +0000

    fix: hide logo in modal display

--- src/components/SpMenu/index.tsx
+++ src/components/SpMenu/index.tsx
@@ -10,24 +10,27 @@ const SpMenu = () => {
   const { isOpen, onOpen, onClose } = useModal();
 
   return (
-    <div className={`${isOpen ? 'fixed inset-0' : 'fixed'}`}>
+    <>
       {/* sp:bottom-[32px] sp:right-[32px] */}
       <div className="fixed bottom-[4.266666666666667vw] right-[4.266666666666667vw]" onClick={onOpen}>
         <SpResponsiveImage alt="menu" src="/SP/UnderMenu/s01_UnderMenu_CircleIcon.png" className="relative" width={95} height={95} />
       </div>
       {/* sp:bottom-[-2px] sp:left-[-8px] */}
+      {/* <div className={`${isOpen ? '' : 'fixed bottom-[-0.26666667vw] left-[-1.06666667vw]'}`}> */}
       <div className="fixed bottom-[-0.26666667vw] left-[-1.06666667vw]">
         <div className="relative">
           <SpResponsiveImage alt="base" src="/SP/UnderMenu/s01_UnderMenu_LeftBase.png" className="" width={350} height={150} />
           {/* sp:bottom-[16px] sp:left-[21px] */}
           <div className="absolute bottom-[2.13333333vw] left-[2.8vw]">
-            <SpResponsiveImage alt="logo" src="/SP/UnderMenu/s01_UnderMenu_Logo.png" className="" width={270} height={135} />
+            <Link href="/sp" passHref>
+              <SpResponsiveImage alt="logo" src="/SP/UnderMenu/s01_UnderMenu_Logo.png" className="" width={270} height={135} />
+            </Link>
           </div>
         </div>
       </div>
 
       {/* メニューモーダル */}
-      <div className={`${isOpen ? '' : 'invisible'}`}>
+      <div className={`${isOpen ? 'fixed inset-0' : 'invisible fixed'}`}>
         <Background src="/SP/Menu/s00_Menu_BG.png" />
         {/* sp:space-y-[16px] sp:pt-[32px] */}
         <div className="grid justify-items-center space-y-[2.133333333333333vw] pt-[4.26666667vw]">
@@ -57,7 +60,7 @@ const SpMenu = () => {
           <SpResponsiveImage alt="close" src="/SP/Menu/s00_Button_21_Close.png" className="" width={95} height={95} />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
