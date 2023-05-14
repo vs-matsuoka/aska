d74b90830567708678f01ee2709575c35ff6042a
naari3 <naari.named@gmail.com>
2023-05-14 18:23:51 +0900

    feat: キービジュアルをふわふわと浮かせる

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,48 @@
+// prettier-ignore
+const redZoneLead = [
+  false, false, true, true, false, false, true, true, //
+  false, true, false, true, true, false, true, false, //
+  true, false, true, true, false, false, true, true, //
+  false, true, false, true, true, false, true, false, //
+  true, true, false, false, true, true, false, false, //
+  true, false, true, false, false, true, false, true, //
+  false, true, false, true, false, false, true, true, //
+  false, true, false, false, true, false, true, false, //
+  true, true, false, false, true, true, false, false, //
+  true, false, true, false, false, true, false, true, //
+  false, true, false, false, true, true, false, false, //
+  true, false, true, false, false, true, false, true, //
+  false, false, true, true, false, false, true, true, //
+  false, true, false, true, true, false, true, false, //
+  true, false, true, false, true, true, false, false, //
+  true, false, true, true, true, true, true, true, //
+  false,
+];
+
+// prettier-ignore
+const redZoneBass = [
+  false, false, true, true, false, false, true, false, //
+  true, true, false, false, true, true, false, false, //
+  true, true, false, false, true, true, false, true, //
+  false, false, true, true, false, false, true, true, //
+  false,
+];
+
+// @param {boolean[]} beats
+function redZoning(beats) {
+  return beats.map((b, i) => {
+    let key = '';
+    if (i === i.length - 1) {
+      key = '100%';
+    } else {
+      key = `${(i * 100) / (beats.length - 1)}%`;
+    }
+    return {
+      [key]: { transform: b ? 'scale(-1, 1)' : 'scale(1, 1)' }
+    };
+  });
+}
+
 module.exports = {
   mode: 'jit',
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
@@ -21,10 +66,27 @@ module.exports = {
             opacity: '1',
             transform: 'translateX(0)'
           }
+        },
+        'redzone-left': redZoning(redZoneLead),
+        'redzone-right': redZoning(redZoneBass),
+        'keyvisual-float': {
+          '0%': {
+            transform: 'translateY(0px)'
+          },
+          '50%': {
+            transform: 'translateY(20px)'
+          },
+          '100%': {
+            transform: 'translateY(0px)'
+          }
         }
       },
       animation: {
-        'fade-in-from-left': 'fade-in-from-left 0.2s ease-out'
+        'fade-in-from-left': 'fade-in-from-left 0.2s ease-out',
+        'redzone-left': 'redzone-left 11.636s steps(1, jump-start) infinite',
+        'redzone-right': 'redzone-right 11.636s steps(1, jump-start) infinite',
+        'zeus-float': 'keyvisual-float 4s ease-in-out -1s infinite',
+        'amateras-float': 'keyvisual-float 4s ease-in-out infinite'
       }
     }
   },
