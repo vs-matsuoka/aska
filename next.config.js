6c62a98f9b5d1500c7e7af4f7d35ef054086adf5
naari3 <naari.named@gmail.com>
2023-05-14 22:22:58 +0900

    feat: sentry追加

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withExportImages = require('next-export-optimize-images');
 
 /** @type {import('next').NextConfig} */
-module.exports = withExportImages({
+const nextConfig = withExportImages({
   reactStrictMode: true,
   images: {
     // unoptimized: true
@@ -9,6 +9,7 @@ module.exports = withExportImages({
   experimental: {
     scrollRestoration: true
   },
+  output: 'export',
   trailingSlash: true,
   webpack: (config, { isServer }) => {
     if (!isServer) {
@@ -18,3 +19,39 @@ module.exports = withExportImages({
     return config;
   }
 });
+
+// Injected content via Sentry wizard below
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+
+    org: 'naari3',
+    project: 'otomdm-ten',
+
+    authToken: process.env.SENTRY_AUTH_TOKEN ?? undefined
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true
+  }
+);
