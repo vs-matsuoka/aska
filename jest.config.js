673a07b370fe960330b09a92b8e1380209c77585
naari3 <naari.named@gmail.com>
2023-06-03 17:11:46 +0900

    build: Emotionを使用する

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './'
+});
+
 /** @type {import('jest').Config} */
-const config = {
+const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
@@ -9,7 +15,10 @@ const config = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!url-join)']
+  snapshotSerializers: ['@emotion/jest/serializer']
 };
 
-module.exports = config;
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!url-join)']
+});
