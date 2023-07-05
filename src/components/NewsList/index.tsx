5e0d13b916110f157b08701e2172b779ccf55171
naari3 <naari.named@gmail.com>
2023-07-06 01:23:46 +0900

    feat: いくつかのリセットされている要素にスタイルをあてる

--- src/components/NewsList/index.tsx
+++ src/components/NewsList/index.tsx
@@ -4,12 +4,58 @@ import { useNews } from 'hooks/useNews';
 import { News } from 'types/cms-types';
 import {} from '@emotion/react';
 
+const StyledNews = ({ news }: { news: News }) => (
+  <div
+    className={cls('text-white')}
+    css={{
+      '& ul': {
+        listStyleType: 'disc',
+        listStylePosition: 'inside'
+      },
+
+      '& ol': {
+        listStyleType: 'decimal',
+        listStylePosition: 'inside'
+      },
+      '& li': {
+        '> ul': {
+          marginLeft: '20px'
+        },
+        '> ol': {
+          marginLeft: '20px'
+        }
+      },
+      '& a': {
+        color: '#3daee4',
+        textDecorationLine: 'underline'
+        // textDecorationColor: '#0ea5e9',
+        // textDecorationThickness: '1px',
+        // // fontWeight: 700,
+        // '&:hover': {
+        //   textDecorationThickness: '2px'
+        // }
+      },
+      '& blockquote': {
+        borderLeft: '2px solid #ccccd9',
+        paddingLeft: '10px',
+        marginBottom: '0.25rem'
+      },
+      '& hr': {
+        marginBottom: '0.25rem'
+      },
+      '& p': {
+        marginBottom: '0.25rem'
+      }
+    }}
+  >
+    <div dangerouslySetInnerHTML={{ __html: news.content }} />
+  </div>
+);
+
 const News = ({ news }: { news: News }) => (
   <div className={cls('m-4', 'py-4', 'border-b-2')}>
     <h4 className={cls('text-white', 'text-lg', 'font-bold', 'mb-1')}>{news.title}</h4>
-    <div className={cls('text-white', '')}>
-      <div dangerouslySetInnerHTML={{ __html: news.content }} />
-    </div>
+    <StyledNews news={news} />
   </div>
 );
 
