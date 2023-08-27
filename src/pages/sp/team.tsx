72dc430e94644dad8ee107a545b540ed6a9d8522
gobou0516 <gobou.mp0516.vvv@ezweb.ne.jp>
2023-08-27 11:30:24 +0000

    chore: change housou and tokuban

--- src/pages/sp/team.tsx
+++ src/pages/sp/team.tsx
@@ -60,14 +60,14 @@ export default function Team() {
             ))}
           </section>
           <section>
-            <SpTeamHeader>特　番</SpTeamHeader>
-            {team.tokuban.map((member, i) => (
+            <SpTeamHeader>放　送</SpTeamHeader>
+            {team.housou.map((member, i) => (
               <SpTeamItem member={member} key={i} />
             ))}
           </section>
           <section>
-            <SpTeamHeader>放　送</SpTeamHeader>
-            {team.housou.map((member, i) => (
+            <SpTeamHeader>特　番</SpTeamHeader>
+            {team.tokuban.map((member, i) => (
               <SpTeamItem member={member} key={i} />
             ))}
           </section>
