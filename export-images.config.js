e804a44c6f3551e816db176507394dd82974f0cc
naari3 <naari.named@gmail.com>
2023-09-19 21:56:14 +0900

    chore: fix lintstaged for prettier

--- export-images.config.js
+++ export-images.config.js
@@ -5,6 +5,36 @@ const config = {
   convertFormat: [
     ['png', 'webp'],
     ['jpg', 'webp']
+  ],
+  quality: 95,
+  remoteImages: [
+    // TODO: src/const/movies.ts の値と被っているので、そちらを参照するようにしたい
+    'https://img.youtube.com/vi/jb2cFj3wCIA/sddefault.jpg',
+    'https://img.youtube.com/vi/p1cAUzCboN4/sddefault.jpg',
+    'https://img.youtube.com/vi/oWVHp--jqaU/sddefault.jpg',
+    'https://img.youtube.com/vi/TRY8NvI4MFo/sddefault.jpg',
+    'https://img.youtube.com/vi/inL40npy7oA/sddefault.jpg',
+    'https://img.youtube.com/vi/D5l2cW4pDk0/sddefault.jpg',
+    'https://img.youtube.com/vi/fc8YRrj6SNk/sddefault.jpg',
+    'https://img.youtube.com/vi/5KuExLMwxUo/sddefault.jpg',
+    'https://img.youtube.com/vi/y8e8RUs2GJs/sddefault.jpg',
+    'https://img.youtube.com/vi/--3dGlwlGT4/sddefault.jpg',
+    'https://img.youtube.com/vi/DP57HMEp5TE/sddefault.jpg',
+    'https://img.youtube.com/vi/ZSrPxxw_m6M/sddefault.jpg',
+    'https://img.youtube.com/vi/O1iZ01-9K4s/sddefault.jpg',
+    'https://img.youtube.com/vi/Dkr0JMeGtqA/sddefault.jpg',
+    'https://img.youtube.com/vi/hAT73Oi3ZXE/sddefault.jpg',
+    'https://img.youtube.com/vi/Ma7zHBKMPB4/sddefault.jpg',
+    'https://img.youtube.com/vi/Bs_T5RL0vaI/sddefault.jpg',
+    'https://img.youtube.com/vi/qEfHogQDrjQ/sddefault.jpg',
+    'https://img.youtube.com/vi/PGISH8019XM/sddefault.jpg',
+    'https://img.youtube.com/vi/khfXQCZoDHo/sddefault.jpg',
+    'https://img.youtube.com/vi/O5R79zTKgfI/sddefault.jpg',
+    'https://img.youtube.com/vi/6uZ6hL7J8DE/sddefault.jpg',
+    'https://img.youtube.com/vi/zMDb9lUScfU/sddefault.jpg',
+    'https://img.youtube.com/vi/jJ9KwC-NeQw/sddefault.jpg',
+    'https://img.youtube.com/vi/GR3V88L_2LU/sddefault.jpg',
+    'https://img.youtube.com/vi/q65tyDWSCgw/sddefault.jpg'
   ]
 };
 
