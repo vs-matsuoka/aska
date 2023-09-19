6c0058615bcdad597c7991b11cb383d1b6450243
naari3 <naari.named@gmail.com>
2023-09-19 21:50:51 +0900

    chore: only use next lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,6 @@ module.exports = {
     (filenames) =>
       `next lint --fix --file ${filenames
         .map((file) => file.split(process.cwd())[1])
-        .join(' --file ')}`,
-    'prettier --write'
+        .join(' --file ')}`
   ]
 };
